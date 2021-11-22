770ddfdcba15e04791344c49f37c428e9f47f55c
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2021-11-22 23:51:44 +0400

    Added UserWithTokenModel for Combining Token with User details

---
+++ Models/UserWithToken.cs
@@ -0,0 +1,23 @@
+﻿using Bitbucket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bitbucket.Models
+{
+    public class UserWithToken : User
+    {
+
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public UserWithToken(User user)
+        {
+            this.Id = user.Id;
+            this.Email = user.Email;
+            this.Name = user.Name;
+            this.Surname = user.Surname;
+        }
+    }
+}
