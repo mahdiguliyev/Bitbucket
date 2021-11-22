37481a312847aefc6e50f169a85a6e751103250d
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2021-11-22 23:58:47 +0400

    Refactoring UserController for Methods

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,16 @@
 ﻿using Bitbucket.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Bitbucket.Controllers
@@ -20,6 +26,66 @@ namespace Bitbucket.Controllers
         {
             _context = context;
             _jwtsettings = jwtsettings.Value;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserWithToken>> Init([FromBody] User user)
+        {
+            var useR = await _context.Users
+                       .Include(u => u.ListOfLogins)
+                       .Where(u => u.Email == user.Email && u.Password == user.Password).FirstOrDefaultAsync();
+
+            UserWithToken userWithToken = null;
+
+            if (useR != null)
+            {
+                RefreshToken refreshToken = GenerateRefreshToken();
+                useR.RefreshTokens.Add(refreshToken);
+                await _context.SaveChangesAsync();
+
+                userWithToken = new UserWithToken(useR);
+                userWithToken.RefreshToken = refreshToken.Token;
+            }
+
+            if (userWithToken == null)
+            {
+                return NotFound();
+            }
+
+            userWithToken.AccessToken = GenerateAccessToken(useR.Id);
+
+            return userWithToken;
+        }
+        private RefreshToken GenerateRefreshToken()
+        {
+            RefreshToken refreshToken = new RefreshToken();
+
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                refreshToken.Token = Convert.ToBase64String(randomNumber);
+            }
+            refreshToken.ExpiryDate = DateTime.UtcNow.AddMonths(6);
+
+            return refreshToken;
+        }
+        private string GenerateAccessToken(Guid userId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtsettings.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, Convert.ToString(userId))
+                }),
+                Expires = DateTime.UtcNow.AddSeconds(6),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
